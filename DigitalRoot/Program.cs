46a8e30a8d639c961fcf9d7fba9ffb08e2351e3d
SeedPaths-User <lessdismissive@email.com>
2014-05-28 10:28:45 -0600

    Initial check-in
    Uploading to server. This program is incomplete. Details within SP
    Binder pg.43.
    This upload was needed since current laptop is being returned soon

---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void DigitalRoot(string rootThis)
+        {
+            List<int> nums = new List<int>();
+            var inp = rootThis;
+            var count = 0;
+            while(inp.Length>1)
+            {
+                for (int i = 0; i < inp.Length; i++)
+			    {
+			        nums.Add(Convert.ToInt32(inp[i].ToString));
+			    }
+                foreach (int s in nums)
+                {
+                    count += s;
+	{
+		 
+	}
+                
+        }
+    }
+}
